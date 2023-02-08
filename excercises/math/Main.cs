66a261333537af6c80ade9b37dc029348101b8ee
Giganten21 <andreas.bangpedersen@gmail.com>
2023-02-08 11:27:44 +0100

    2342

---
+++ excercises/math/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public static class main{
+
+	public static void Main(){
+	double sqrt2=Sqrt(2.0);
+	Write($"sqrt2^2 = {sqrt2*sqrt2}\n");
+
+	Write($"2^1/5 = {Pow(2,0.2)}\n");
+
+	Write($"e^pi = {Pow(E,PI)}\n");
+
+	Write($"pi^e = {Pow(PI,E)}\n");
+	}
+}
